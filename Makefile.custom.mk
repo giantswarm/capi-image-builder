e1c1c347993fd57e8d5f2f889506eddc3357f379
Marcus Noble <AverageMarcus@users.noreply.github.com>
2022-07-18 15:23:51 +0100

    Added missing workflow files (#3)

---
+++ Makefile.custom.mk
@@ -0,0 +1,7 @@
+
+ensure-schema-gen:
+	@helm schema-gen --help &>/dev/null || helm plugin install https://github.com/mihaisee/helm-schema-gen.git
+
+.PHONY: schema-gen
+schema-gen: ensure-schema-gen ## Generates the values schema file
+	@cd helm/capi-image-builder && helm schema-gen values.yaml > values.schema.json
